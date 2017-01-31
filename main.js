3f2e83268c029e59a1703b0baa5783b40ee7b9f6
livty <tetyana.tykhomyrova@metropolia.fi>
2017-01-31 18:22:28 +0200

    updates

--- main.js
+++ main.js
@@ -3,13 +3,16 @@
 import Exponent from 'exponent';
 //import a library to create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 //create a Component
 const App = () => (
-
+  <View>
   <Header headerText={'Albums'} />
+  <AlbumList />
+  </View>
   );
 
 
